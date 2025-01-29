3215341622180f141e5c03c57b8a08238755e89a
oscarrep <oscar.bcn.95@gmail.com>
2025-01-29 13:18:30 +0100

    creating budget-list home panel modal welcome components + budget service

---
+++ src/app/budgets-list/budgets-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budgets-list',
+  imports: [],
+  templateUrl: './budgets-list.component.html',
+  styleUrl: './budgets-list.component.sass'
+})
+export class BudgetsListComponent {
+
+}
---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel',
+  imports: [],
+  templateUrl: './panel.component.html',
+  styleUrl: './panel.component.sass'
+})
+export class PanelComponent {
+
+}
