4484ebe0002a3596aaac8033056e5a3d5af1b722
oscarrep <oscar.bcn.95@gmail.com>
2025-02-20 12:47:06 +0100

    ordering buttons + order by date functionality

--- src/app/budgets-list/budgets-list.component.ts
+++ src/app/budgets-list/budgets-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BudgetService } from '../../../services/budget.service';
 import { HomeComponent } from '../home/home.component';
+import { BudgetRequest } from '../interfaces/budget-request';
 
 
 @Component({
@@ -10,7 +11,35 @@ import { HomeComponent } from '../home/home.component';
   templateUrl: './budgets-list.component.html',
   styleUrl: './budgets-list.component.scss'
 })
-export class BudgetsListComponent { 
+export class BudgetsListComponent {
+
+  private originalOrder: { item: BudgetRequest, index: number }[] = [];
+  toggleDate: boolean = false;
+  togglePrice: boolean = false;
+  toggleAbc: boolean = false;
+
 
   constructor(public budgetList: BudgetService, public home: HomeComponent) { }
+
+  ngOnInit() {
+    this.originalOrder = this.budgetList.requestedBudgets().map((item, index) => ({ item, index }));
+  }
+
+  orderDate() {
+    if (this.toggleDate === true) {
+      this.toggleDate = false;
+      const sortedBudgets = this.originalOrder.sort((a, b) => a.index - b.index).map(entry => entry.item);
+      this.budgetList.requestedBudgets.set(sortedBudgets);
+    } else if (this.toggleDate === false) {
+      this.toggleDate = true;
+      const sortedBudgets = this.originalOrder.sort((a, b) => b.index - a.index).map(entry => entry.item);
+      this.budgetList.requestedBudgets.set(sortedBudgets);
+    }
+  }
+  orderPrice() {
+
+  }
+  orderAbc() {
+
+  }
 }
