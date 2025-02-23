7f031bd80ead5ee6bf367b30d6be449dd507301f
oscarrep <oscar.bcn.95@gmail.com>
2025-02-23 17:29:26 +0100

    alphabetical order

--- src/app/budgets-list/budgets-list.component.ts
+++ src/app/budgets-list/budgets-list.component.ts
@@ -69,7 +69,20 @@ export class BudgetsListComponent implements OnInit {
   }
 
   orderAbc() {
+    if (this.toggleAbc === true) this.ascendingAbc();
+    else if (this.toggleAbc === false) this.descendingAbc();
+  }
 
+  ascendingAbc() {
+    this.toggleAbc = false;
+    const sortedBudgets = [...this.budgetList.requestedBudgets()].sort((a, b) => a.name.localeCompare(b.name));
+    this.budgetList.requestedBudgets.set(sortedBudgets);
+  }
+
+  descendingAbc() {
+    this.toggleAbc = true;
+    const sortedBudgets = [...this.budgetList.requestedBudgets()].sort((a, b) => b.name.localeCompare(a.name));
+    this.budgetList.requestedBudgets.set(sortedBudgets);
   }
 
   getIndex(budget: BudgetRequest): number { return this.budgetList.requestedBudgets().indexOf(budget); }
