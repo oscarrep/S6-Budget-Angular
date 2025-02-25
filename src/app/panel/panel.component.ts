6ffcf8224a20b7e23b99b3f2e267167ec8f270aa
oscarrep <oscar.bcn.95@gmail.com>
2025-02-25 12:55:01 +0100

    adding/removing pages/languages updates url

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { BudgetService } from '../../../services/budget.service';
+import { HomeComponent } from '../home/home.component';
 
 
 @Component({
   selector: 'app-panel',
-  imports: [],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.scss'
 })
@@ -17,18 +17,20 @@ export class PanelComponent {
   popupP: boolean = false;
   popupL: boolean = false;
 
-  constructor(public budgetList: BudgetService) { }
+  constructor(public budgetList: BudgetService, public home: HomeComponent) { }
 
   addPage(): void {
     this.add = true;
     this.budgetList.pages.update(pages => pages + 1);
     this.budgetList.calculateWebTotal(true);
+    this.home.updateUrl();
   }
 
   addLanguage(): void {
     this.add = true;
     this.budgetList.languages.update(languages => languages + 1);
     this.budgetList.calculateWebTotal(true);
+    this.home.updateUrl();
   }
 
   removePage(): void {
@@ -36,6 +38,7 @@ export class PanelComponent {
       this.add = false;
       this.budgetList.pages.update(pages => pages > 0 ? pages - 1 : 0);
       this.budgetList.calculateWebTotal(false);
+      this.home.updateUrl();
     }
   }
 
@@ -44,6 +47,7 @@ export class PanelComponent {
       this.add = false;
       this.budgetList.languages.update(languages => languages > 0 ? languages - 1 : 0);
       this.budgetList.calculateWebTotal(false);
+      this.home.updateUrl();
     }
   }
 
